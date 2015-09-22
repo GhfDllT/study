17c1f2cb4d81b81e181b1c7a63132018aaf1dd1b
GhfDllT <ghfdllt@gmail.com>
2015-09-23 00:18:48 +0300

    create iofs proj

---
+++ IOFS/iofs1/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace iofs1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.действиеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.проверитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.словарьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.действиеToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1249, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // действиеToolStripMenuItem
+            // 
+            this.действиеToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.проверитьToolStripMenuItem,
+            this.словарьToolStripMenuItem});
+            this.действиеToolStripMenuItem.Name = "действиеToolStripMenuItem";
+            this.действиеToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+            this.действиеToolStripMenuItem.Text = "Действие";
+            // 
+            // проверитьToolStripMenuItem
+            // 
+            this.проверитьToolStripMenuItem.Name = "проверитьToolStripMenuItem";
+            this.проверитьToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.проверитьToolStripMenuItem.Text = "Проверить";
+            this.проверитьToolStripMenuItem.Click += new System.EventHandler(this.проверитьToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Text files (*.txt)|*.txt";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 27);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(1222, 151);
+            this.textBox1.TabIndex = 1;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Location = new System.Drawing.Point(12, 200);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(543, 439);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "зависимость вхождения слова";
+            title1.Name = "Title1";
+            title1.Position.Auto = false;
+            title1.Position.Height = 2F;
+            title1.Position.Width = 94F;
+            title1.Position.X = 3F;
+            title1.Position.Y = 97F;
+            title1.Text = "Зависимость частоты вхождения слова от количества слов с данной частотой";
+            this.chart1.Titles.Add(title1);
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            this.chart2.Location = new System.Drawing.Point(588, 200);
+            this.chart2.Name = "chart2";
+            this.chart2.Size = new System.Drawing.Size(646, 439);
+            this.chart2.TabIndex = 3;
+            this.chart2.Text = "chart2";
+            title2.Name = "Title1";
+            title2.Position.Auto = false;
+            title2.Position.Height = 3F;
+            title2.Position.Width = 94F;
+            title2.Position.X = 3F;
+            title2.Position.Y = 97F;
+            title2.Text = "График зависимости ранга частоты от частоты вхождения слова";
+            this.chart2.Titles.Add(title2);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 691);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // словарьToolStripMenuItem
+            // 
+            this.словарьToolStripMenuItem.Name = "словарьToolStripMenuItem";
+            this.словарьToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.словарьToolStripMenuItem.Text = "словарь";
+            this.словарьToolStripMenuItem.Click += new System.EventHandler(this.словарьToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1249, 752);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolStripMenuItem действиеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem проверитьToolStripMenuItem;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem словарьToolStripMenuItem;
+    }
+}
+
---
+++ IOFS/iofs1/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using MoreLinq;
+
+namespace iofs1
+{
+    public partial class Form1 : Form
+    {
+        const string toDelete = ",.!?...;:-(){}'\"\r\n«»1234567890—";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() != DialogResult.OK) return;
+
+            Text = File.OpenText(openFileDialog1.FileName).ReadToEnd();
+            toDelete.ForEach(c => Text = Text.Replace(c, ' '));
+        }
+
+        private string Text
+        {
+            get { return textBox1.Text; }
+            set { textBox1.Text = value; }
+        }
+
+        private void проверитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            IEnumerable<string> words = GetWordsInText().ToArray();
+            string[] textWords = GetTextByWords().ToArray();
+
+            var frequencies = words.ToDictionary(w => w, w => textWords.Count(tw => tw.Contains(w)));
+            var rangs = frequencies
+                       .GroupBy(p => p.Value)
+                       .OrderByDescending(g => g.Key)
+                       .ToArray();
+
+            Series series1 = new Series();
+            Series series2 = new Series();
+
+            int index = 1;
+            rangs.ForEach(r =>
+                              {
+                                  series1.Points.AddXY(r.Key, r.Count());
+                                  series2.Points.AddXY(index, r.Key);
+                                  index++;
+                              });
+
+            chart1.Series.Clear();
+            chart1.Series.Add(series1);
+            chart2.Series.Clear();
+            chart2.Series.Add(series2);
+
+            double C = rangs.SelectMany(r => r.Select((p, i) => (p.Value * i) / (double)textWords.Length))
+                            .Average();
+            label1.Text = string.Format("Постоянная Зиппа: {0}", C);
+        }
+
+        private IEnumerable<string> GetWordsInText()
+        {
+            return GetTextByWords().Distinct();
+        }
+
+        private IEnumerable<string> GetTextByWords()
+        {
+            return Text.Split(' ')
+                       .Select(w => w.Trim())
+                       .Where(w => !toDelete.Contains(w));
+        }
+
+        private void словарьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using(var outFile = File.CreateText(@"C:\Users\s.valyushitsky\Desktop\IOFS\iofs1\dict_ru1.txt"))
+            {
+                var forms = new List<string>();
+
+                File.ReadLines(@"C:\Users\s.valyushitsky\Desktop\IOFS\iofs1\dict_ru.txt")
+                    .Where(str => !string.IsNullOrWhiteSpace(str))
+                    .Select(str => str.TrimEnd())
+                    .ForEach(str =>
+                                 {
+                                     if (!str.StartsWith(" "))
+                                     {
+                                         outFile.WriteLine(string.Join("|", forms.Select(f => f.Trim())));
+                                         forms.Clear();
+                                     }
+
+                                     forms.Add(str);
+                                 });
+            }
+        }
+    }
+}
