9a87e2d1411bc509f2f125d5ed667e017e687aa6
GhfDllT <ghfdllt@gmail.com>
2022-04-21 18:50:47 +0300

    adde signalR ts

---
+++ SignalRWebpack/Hubs/ChatHub.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRWebpack.Hubs
+{
+    public class ChatHub: Hub
+    {
+        public Task NewMessage(long username, string message)
+            => Clients.All.SendAsync("messageReceived", username, message);
+    }
+}
